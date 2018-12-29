c839eef79e386fe397ecde98bd240803a3dca1d4
alejandro <alejandro.garcia@wizeline.com>
2018-12-28 21:00:38 -0600

    wip

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -15,6 +15,15 @@ func get_input():
     if Input.is_action_pressed('up'):
         velocity.y -= 1
     velocity = velocity.normalized() * speed
+    var mouse_pos = get_global_mouse_position()
+    var distance = (mouse_pos-position).length()
+    var new_angle = (mouse_pos-position).angle() + PI
+    var x = distance * cos(new_angle) + mouse_pos.x
+    var y = distance * sin(new_angle) + mouse_pos.y
+    self.look_at(Vector2(x, y))
+    print("new angle ", new_angle)
+    print("new pos", Vector2(x, y))
+
 
 func _physics_process(delta):
     get_input()
