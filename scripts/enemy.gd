b90fcea6b39ff07f8584e5299fc9c89649fc3924
alejandro <alejandro.garcia@wizeline.com>
2018-12-31 00:07:46 -0600

    added basic enemy behavior

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
+export (int) var speed = 300
+
 func _ready():
 	set_process(true)
 	pass
 
 func _process(delta):
-	
-	pass
+	var player = get_node("../../player")
+	var direction = player.position - position
+	var dist = sqrt(direction.x*direction.x + direction.y*direction.y)
+	if dist > 16:
+		position += direction.normalized() * speed * delta
