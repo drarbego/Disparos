a82517ce0717e2bbf30296a7af334ed7e9833aad
alejandro <alejandro.garcia@wizeline.com>
2018-12-30 23:46:42 -0600

    added spawner

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -2,21 +2,28 @@ extends Node
 
 const Enemy = preload("res://scenes/enemy.tscn")
 
+func get_spawners():
+	var spawners = []
+	var children = get_children()
+	for child in children:
+		if child.is_class("Node2D"):
+			spawners.append(child)
+	return spawners
+
 func spawn():
+	var spawners = get_spawners()
+	
 	randomize()
+	var spawner = spawners[randi()%spawners.size()]
+	
+
 	var enemy = Enemy.instance()
-	var enemy_position = Vector2()
-	enemy_position.x = [0, get_viewport().size.x][randi()%2]
-	enemy_position.y = [0, get_viewport().size.y][randi()%2]
-	enemy.position = enemy_position
-	print(enemy_position)
-	get_node("container").add_child(enemy)
+	enemy.position = spawner.position
+	self.add_child(enemy)
 
 func _ready():
 	spawn()
-	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _on_Timer_timeout():
+	spawn()
+
