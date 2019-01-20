e2dad196de2354ba865cced76bc4129bf8b24b7f
alejandro <alejandro.garcia@wizeline.com>
2019-01-20 09:26:17 -0600

    moved files and started implementig grid

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -16,7 +16,13 @@ func freeze():
 	var timer = get_node('freeze')
 	timer.wait_time = freezeTime
 	timer.start()
+	print(get_position_in_grid())
 
+func get_position_in_grid():
+	var cell_size = 10
+	var cell_width = get_viewport().size.x / cell_size
+	var cell_height = get_viewport().size.y / cell_size
+	return Vector2(int(position.x / cell_width), int(position.y / cell_height))
 
 func _on_freeze_timeout():
 	isFrozen = false
