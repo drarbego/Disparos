17023d6ff6dc235eb72b9a002b7ccfde75e45f9e
alejandro <alejandro.garcia@wizeline.com>
2019-02-09 21:12:07 -0600

    wip

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -6,7 +6,14 @@ var direction = Vector2()
 func _ready():
 	direction = (get_global_mouse_position() - position).normalized()
 
+func isOutOfTheScreen():
+	var isInsideX = position.x <= get_viewport().size.x && position.x >= 0
+	var isInsideY = position.y <= get_viewport().size.y && position.y >= 0
+	return !(isInsideX && isInsideY)
+
 func _process(delta):
+	if isOutOfTheScreen():
+		queue_free()
 	position += direction * speed * delta
 
 func _on_bullet_area_entered(area):
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,25 +4,44 @@ export(int) var speed
 export(float) var freezeTime
 var isFrozen = false
 
+var defaultTexture = preload('res://sprites/enemy.png')
+var frozenTexture = preload('res://sprites/enemy__frozen.png')
+
 func _process(delta):
 	if not isFrozen:
 		var player = get_node('/root/world/player')
 		var direction = (player.position - position).normalized()
 		position += direction * speed * delta
 
+func checkEnemyIsFrozen():
+	# TODO this function will check if frozen enemies
+	# are alligned, if so, they will disapear and score will
+	# increment
+	var frozenEnemies = 0
+	var enemies = get_tree().get_nodes_in_group('enemies')
+	for enemy in enemies:
+		if enemy.isFrozen:
+			frozenEnemies += 1
+	print('frozen enemies = ', frozenEnemies)
+
+
 func freeze():
-	print('I have to freeze')
 	isFrozen = true
+	position = get_position_in_grid()
 	var timer = get_node('freeze')
+	get_node('sprite').texture = frozenTexture
+	checkEnemyIsFrozen()
 	timer.wait_time = freezeTime
 	timer.start()
-	print(get_position_in_grid())
 
 func get_position_in_grid():
-	var cell_size = 10
-	var cell_width = get_viewport().size.x / cell_size
-	var cell_height = get_viewport().size.y / cell_size
-	return Vector2(int(position.x / cell_width), int(position.y / cell_height))
+	var numberOfCells = 10
+	var cellWidth = get_viewport().size.x / numberOfCells
+	var cellHeight = get_viewport().size.y / numberOfCells
+	var x = int(position.x / cellWidth) * cellWidth + cellWidth/2
+	var y = int(position.y / cellHeight) * cellHeight + cellHeight/2
+	return Vector2(x, y)
 
 func _on_freeze_timeout():
 	isFrozen = false
+	get_node('sprite').texture = defaultTexture
